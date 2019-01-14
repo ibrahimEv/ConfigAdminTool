d066870179398499316e865d490ea5daa000a5eb
mayuresh-EVH <43809791+mayuresh-EVH@users.noreply.github.com>
2019-01-14 17:26:15 +0530

    Added loading and exit button

--- ConfigToolWPF/MainWindow.xaml.cs
+++ ConfigToolWPF/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -44,6 +45,7 @@ namespace ConfigToolWPF
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            ShowLoading();
             // Configure open file dialog box
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
             dlg.DefaultExt = ".xlsx"; // Default file extension
@@ -57,14 +59,18 @@ namespace ConfigToolWPF
             {
                 // Open document
                 string excelFileName = dlg.FileName;
+                TxtPRNumber.Text = Regex.Match(excelFileName, @"\d+").Value;
                 _excelHelper.LoadWorkBook(excelFileName);
                 CmbWorkSheet.ItemsSource = _excelHelper.GetAllWorkSheetNames();
 
             }
+
+            HideLoading();
         }
 
         private async void CmbWorkSheet_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            ShowLoading();
             string headBranchName = ConfigurationManager.AppSettings["HeadBranchName"];
             int index = CmbWorkSheet.SelectedIndex;
             string tableName = _excelHelper.SelectWorkSheet(index + 1);
@@ -78,20 +84,24 @@ namespace ConfigToolWPF
             List<string> sqlFromExcel = _excelHelper.GetSqlFromCurrentSheet(columnMappings);
 
             MergedFile = _mergeFile.Merge(contentGithubFile, sqlFromExcel);
+            HideLoading();
         }
 
         private async void CmbRepository_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            ShowLoading();
             string githubUserToken = ConfigurationManager.AppSettings["GithubUserToken"];
             _githubHelper = new GithubHelper(CmbRepository.SelectedValue.ToString(), githubUserToken);
             List<string> allBranches = await _githubHelper.GetAllBranches();
             CmbBranches.ItemsSource = allBranches;
             List<string> reviewers = await _githubHelper.GetAllCollaborators();
             ListReviewers.ItemsSource = reviewers;
+            HideLoading();
         }
 
         private async void BtnCreatePR_OnClick(object sender, RoutedEventArgs e)
         {
+            ShowLoading();
             string headBranchName = CmbBranches.SelectedValue.ToString();
             string newBranchName = TxtNewBranchName.Text;
             List<string> reviewerNames = new List<string>();
@@ -99,11 +109,12 @@ namespace ConfigToolWPF
             {
                 reviewerNames.Add(reviewer.ToString());
             }
-            
+
             //var t = await _githubHelper.CreateBranch(headBranchName, newBranchName);
             //var t1 = await _githubHelper.UpdateFile(GithubFilePath, string.Join("\n", MergedFile), newBranchName);
-            //int prNumber = await _githubHelper.CreatePullRequest("New PR " + newBranchName, headBranchName, newBranchName);
+            int prNumber = await _githubHelper.CreatePullRequest(TxtPRNumber.Text, headBranchName, newBranchName);
             //int temp = await _githubHelper.AddReviewerToPullRequest(prNumber, reviewerNames);
+            HideLoading();
         }
 
         private void MainWindow_OnClosed(object sender, EventArgs e)
@@ -114,6 +125,7 @@ namespace ConfigToolWPF
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             SubWindow window = new SubWindow();
+
             window.TxtMergedFile.Text = string.Join("\n", MergedFile);
             window.Show();
         }
@@ -123,10 +135,15 @@ namespace ConfigToolWPF
             this.Cursor = Cursors.Wait;
             this.Opacity = 0.8;
         }
-        private void ResetLoading()
+        private void HideLoading()
         {
             this.Cursor = Cursors.Arrow;
             this.Opacity = 1;
+        }
+
+        private void BtnExit_OnClick(object sender, RoutedEventArgs e)
+        {
+           Application.Current.Shutdown();
         }
     }
 }
